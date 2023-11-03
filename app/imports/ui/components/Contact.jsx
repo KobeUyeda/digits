fb24d91b5ae916a093b30421442ee3d6953dba30
kobe <kobeuyeda@outlook.com>
2023-11-02 21:15:05 -1000

    contact forms finished in 24 minutes.

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const Contact = ({ contact }) => (
@@ -21,6 +22,7 @@ const Contact = ({ contact }) => (
       <footer className="blockquote-footer">
         {contact.owner}
       </footer>
+      <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
 );
@@ -34,6 +36,7 @@ Contact.propTypes = {
     address: PropTypes.string,
     description: PropTypes.string,
     owner: PropTypes.string,
+    _id: PropTypes.string,
   }).isRequired,
 };
 
